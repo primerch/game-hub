b31d06da6d4e02c6dc363a37756f7bad607a3d1a
Reacher <macryze@icloud.com>
2025-08-12 13:03:51 +0800

    Implement responsive grid layout with navigation, aside, and main sections in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="grid [grid-template-areas:'nav''main'] lg:[grid-template-areas:'nav_nav''aside_main']">
+        <div className="bg-orange-500 [grid-area:nav]">Nav</div>
+        <div className="hidden bg-yellow-500 [grid-area:aside] lg:block">
+          Aside
+        </div>
+        <div className="bg-blue-500 [grid-area:main]">Main</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
