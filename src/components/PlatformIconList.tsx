02aeca31200bcef36e26ace25476963099f23cb8
Reacher <macryze@icloud.com>
2025-08-13 17:06:41 +0800

    Add platform icons to `PlatformIconList` using `react-icons` for improved visual representation

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,3 +1,12 @@
+import {
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { BsNintendoSwitch } from "react-icons/bs";
+
 export interface Platform {
   platform: {
     id: number;
@@ -9,11 +18,24 @@ interface Props {
   parent_platforms: Platform[];
 }
 
+function slugToIcon(pstring: string) {
+  if (pstring === "pc") return <FaWindows className="mr-2 inline" />;
+  if (pstring === "playstation")
+    return <FaPlaystation className="mr-2 inline" />;
+  if (pstring === "nintendo")
+    return <BsNintendoSwitch className="mr-2 inline" />;
+  if (pstring === "apple") return <FaApple className="mr-2 inline" />;
+  if (pstring === "xbox") return <FaXbox className="mr-2 inline" />;
+  if (pstring === "linux") return <FaLinux className="mr-2 inline" />;
+}
+
 const PlatformIconList = ({ parent_platforms }: Props) => {
   return (
     <div>
       {parent_platforms.map(({ platform }) => (
-        <span key={platform.id}>{platform.slug}</span>
+        <>
+          <span key={platform.id}>{slugToIcon(platform.slug)}</span>
+        </>
       ))}
     </div>
   );
