d1ea235771c8a93621f62fbb0b5d30a02880bce3
Reacher <macryze@icloud.com>
2025-08-13 12:38:02 +0800

    Adjust icon and logo sizes for improved UI consistency in NavBar and ColorModeSwitch components

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -6,7 +6,7 @@ const ColorModeSwitch = () => {
 
       {/* sun icon */}
       <svg
-        className="swap-on h-10 w-10 fill-current"
+        className="swap-on h-8 w-8 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
@@ -15,7 +15,7 @@ const ColorModeSwitch = () => {
 
       {/* moon icon */}
       <svg
-        className="swap-off h-10 w-10 fill-current"
+        className="swap-off h-8 w-8 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import ColorModeSwitch from "./ColorModeSwitch.tsx";
 const NavBar = () => {
   return (
     <div className="flex justify-between p-2.5">
-      <img src={logo} alt="" className="w-16" />
+      <img src={logo} alt="" className="w-12" />
       <ColorModeSwitch />
     </div>
   );
