42236782733a6c910878aca56cf593e022f2fe84
Reacher <macryze@icloud.com>
2025-08-13 12:34:42 +0800

    Refactor GameGrid layout to use a responsive grid and improve game card styling

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,13 +1,17 @@
 import type { Game } from "../hooks/useGames";
 
+interface Props {
+  game: Game;
+}
+
 const GameCard = ({ name, background_image }: Game) => {
   return (
-    <div className="card card-border bg-base-100 w-96 shadow-sm">
+    <div className="card card-border bg-base-100 m-2.5 shadow-sm">
       <figure>
         <img src={background_image} alt="Game Background Image" />
       </figure>
       <div className="card-body">
-        <div className="card-title">{name}</div>
+        <div className="card-title text-2xl">{name}</div>
       </div>
     </div>
   );
