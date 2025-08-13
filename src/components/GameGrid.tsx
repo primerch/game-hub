55f3852b0e7ce3d1172fe0a3202e5ef5a58b2b16
Reacher <macryze@icloud.com>
2025-08-13 12:36:11 +0800

    Remove commented-out old game list code from GameGrid component.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,18 +8,6 @@ const GameGrid = () => {
     <>
       {error && <p className="text-error">{error}</p>}
 
-      {/*<ul>*/}
-      {/*  {games.map((game) => (*/}
-      {/*    <li key={game.id}>*/}
-      {/*      <GameCard*/}
-      {/*        id={game.id}*/}
-      {/*        name={game.name}*/}
-      {/*        background_image={game.background_image}*/}
-      {/*      />*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
-
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
         {games.map((game) => (
           <GameCard
