80b630aa6734f9461b9129b90281d3ab4b97d1c6
Reacher <macryze@icloud.com>
2025-08-14 15:02:05 +0800

    Add loading state to `GenreList` with spinner; update `useGenres` to return `loading` attribute.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,26 +1,30 @@
 import useGenres from "../hooks/useGenres.ts";
 
 const GenreList = () => {
-  const { data, error } = useGenres();
+  const { data, error, loading } = useGenres();
 
   return (
     <>
       {error && <p className="text-error">{error}</p>}
-      <ul className="list bg-base-100 rounded-box shadow-md">
-        {data.map((genre) => (
-          <li className="list-row" key={genre.id}>
-            <div>
-              <img
-                className="rounded-box size-10"
-                src={genre.image_background}
-              />
-            </div>
-            <div>
-              <a href="#">{genre.name}</a>
-            </div>
-          </li>
-        ))}
-      </ul>
+
+      {loading && <span className="loading loading-dots loading-xl"></span>}
+      {!loading && (
+        <ul className="list bg-base-100 rounded-box shadow-md">
+          {data.map((genre) => (
+            <li className="list-row" key={genre.id}>
+              <div>
+                <img
+                  className="rounded-box size-10"
+                  src={genre.image_background}
+                />
+              </div>
+              <div>
+                <a href="#">{genre.name}</a>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
