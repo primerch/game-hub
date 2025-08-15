ecdc75fcf153770d8d1155b684a4e71c00d55a1e
Reacher <macryze@icloud.com>
2025-08-15 16:59:28 +0800

    Refactor `PlatformSelector` to simplify data handling and remove unnecessary logging.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,24 +1,17 @@
 import usePlatforms from "../hooks/usePlatforms.ts";
 
 const PlatformSelector = () => {
-  const { data, loading, error } = usePlatforms();
-  console.log(data);
+  const { data, error } = usePlatforms();
 
-  const platforms = data.filter((platform) => ({
-    name: platform.name,
-    slug: platform.slug,
-  }));
-
-  const platform_names = platforms.map((platform) => platform.name);
-  console.log(platform_names);
+  if (error) return null;
 
   return (
     <details className="dropdown">
       <summary className="btn m-1">Platforms</summary>
       <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-        {platform_names.map((platform_name) => (
-          <li key={platform_name}>
-            <a href="">{platform_name}</a>
+        {data.map((platform) => (
+          <li key={platform.id}>
+            <button>{platform.name}</button>
           </li>
         ))}
       </ul>
