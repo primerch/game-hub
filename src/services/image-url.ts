443a66d1ed3a616bcfea9aa6d40c7dc458d4acd5
Reacher <macryze@icloud.com>
2025-08-16 18:37:10 +0800

    Add null check for `url` in `getCroppedImageUrl` and simplify optional chaining usage

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -2,13 +2,15 @@ const getCroppedImageUrl = (url: string | null) => {
   const width = 600;
   const height = 400;
 
+  if (!url) return "";
+
   return (
-    url?.slice(0, url.indexOf("/games")) +
+    url.slice(0, url.indexOf("/games")) +
     "/crop/" +
     width +
     "/" +
     height +
-    url?.slice(url?.indexOf("/games"))
+    url.slice(url.indexOf("/games"))
   );
 };
 
