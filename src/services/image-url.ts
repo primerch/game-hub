6999532c917a38c4b3b432b66afdfc34df25c28f
Reacher <macryze@icloud.com>
2025-08-17 00:24:48 +0800

    Add fallback placeholder image for `getCroppedImageUrl` when `url` is null.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,10 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string | null) => {
   const width = 600;
   const height = 400;
 
-  if (!url) return "";
+  if (!url) return noImage;
 
   return (
     url.slice(0, url.indexOf("/games")) +
