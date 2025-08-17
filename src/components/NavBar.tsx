7040a8e7d3c4e5715ab9ceddc92b3856e6e141f7
Reacher <macryze@icloud.com>
2025-08-17 10:43:01 +0800

    Add `SearchInput` component and integrate it into `NavBar`.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch.tsx";
+import SearchInput from "./SearchInput.tsx";
 
 const NavBar = () => {
   return (
     <div className="flex justify-between p-2.5">
       <img src={logo} alt="" className="w-12" />
+      <SearchInput />
       <ColorModeSwitch />
     </div>
   );
