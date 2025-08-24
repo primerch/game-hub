b0ee23838baf5caba458542d745de3f992e58120
Reacher <macryze@icloud.com>
2025-08-24 19:59:13 +0800

    Refactor main.tsx to remove ReactQueryDevtools and fix imports
    Removed ReactQueryDevtools to updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { QueryClient } from "@tanstack/query-core";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 /**========================================================================
  *                           main.tsx
@@ -14,18 +13,17 @@ const queryClient = new QueryClient({});
 // This code is only for TypeScript
 declare global {
   interface Window {
-    __TANSTACK_QUERY_CLIENT__: import("@tanstack/query-core").QueryClient;
+    __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient;
   }
 }
 
 // This code is for all users
 window.__TANSTACK_QUERY_CLIENT__ = queryClient;
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
