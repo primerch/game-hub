9fffc6c595dbb22bfbc3cef89e54b74515ec8def
Reacher <macryze@icloud.com>
2025-08-24 10:04:45 +0800

    Comment out unused `useGenres` hook implementation `useGenres` hook and related code commented out, likely due to it being temporarily unused awaiting updates. This change prevents unnecessary code execution while

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,24 +1,26 @@
-import apiClient from "@/services/api-client";
-import { useQuery } from "@tanstack/react-query";
+// import apiClient from "@/services/api-client";
+// import { useQuery } from "@tanstack/react-query";
 
-export interface Genre {
-  id: number;
-  name: string;
-  slug: string;
-  image_background: string;
-}
+// export interface Genre {
+//   id: number;
+//   name: string;
+//   slug: string;
+//   image_background: string;
+// }
 
-interface GenreResponse {
-  count: number;
-  results: Genre[];
-}
+// interface GenreResponse {
+//   count: number;
+//   results: Genre[];
+// }
 
-const useGenres = () => {
-  return useQuery<Genre[], Error>({
-    queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<GenreResponse>("/genres").then((res) => res.data.results),
-  });
-};
+// const useGenres = () => {
+//   return useQuery<Genre[], Error>({
+//     queryKey: ["genres"],
+//     queryFn: () =>
+//       apiClient.get<GenreResponse>("/genres").then((res) => res.data.results),
+//   });
+// };
 
-export default useGenres;
+// export default useGenres;
+
+
