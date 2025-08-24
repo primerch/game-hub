91b2001905b6b181187e95b546a58cf9a4b44705
Reacher <macryze@icloud.com>
2025-08-24 18:49:15 +0800

    Refactor platform toplaced custom fetching logic with for improved state management and. Updated the `PlatformSelector` component to reflect API response structure changes and adjusted error consistency.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,28 +1,27 @@
-import type { GameQuery } from "../App.tsx";
-import usePlatforms from "../hooks/usePlatforms.ts";
-import type { Platform } from "./PlatformIconList.tsx";
+import type { GameQuery } from '../App.tsx';
+import usePlatforms from '../hooks/usePlatforms.ts';
+import type { Platform } from './PlatformIconList.tsx';
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: GameQuery["platform"];
+  selectedPlatform: GameQuery['platform'];
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  const { data, error } = usePlatforms();
-  console.log(data);
+  const { data, isError } = usePlatforms();
 
-  if (error) return null;
+  if (isError) return null;
 
   return (
     <div className="dropdown dropdown-hover">
       <div tabIndex={0} role="button" className="btn m-1">
-        {selectedPlatform?.name || "Platform"}
+        {selectedPlatform?.name || 'Platform'}
       </div>
       <ul
         tabIndex={0}
         className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
       >
-        {data?.map((platform) => (
+        {data?.results.map((platform) => (
           <li key={platform.id}>
             <button onClick={() => onSelectPlatform(platform)}>
               {platform.name}
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,7 @@
-import platforms from '@/data/platforms.ts';
-import useData from './useData.ts';
+import platforms from '@/data/platforms';
+import apiClient from '@/services/api-client';
+import { useQuery } from '@tanstack/react-query';
+import type { FetchResponse } from './useData';
 
 interface Platform {
   id: number;
@@ -7,13 +9,16 @@ interface Platform {
   slug: string;
 }
 
-const staleTime = 24 * 60 * 60 * 1000;
-const usePlatforms = () =>
-  useData<Platform>(
-    '/platforms/lists/parents',
-    undefined,
-    staleTime,
-    platforms,
-  );
+const usePlatforms = () => {
+  return useQuery<FetchResponse<Platform>>({
+    queryKey: ['platforms'],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>('/platforms/lists/parents')
+        .then((res) => res.data),
+    initialData: { count: platforms.length, results: platforms },
+    staleTime: 24 * 60 * 60 * 1000,
+  });
+};
 
 export default usePlatforms;
