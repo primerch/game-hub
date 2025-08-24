268f4d4087f24390255ab8aaee7b78e5a6dbccf2
Reacher <macryze@icloud.com>
2025-08-24 10:50:56 +0800

    Refactor streamline and simplify implementationReplaced custom data fetching logic in `useData` and `useGenres` with React Query for better code maintainability and efficiency. Removed commented-out legacy code to improve code clarity and reduce clutter

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,60 +1,6 @@
-// import { useEffect, useState } from "react";
-// import apiClient, { CanceledError } from "../services/api-client.ts";
-// import type { AxiosRequestConfig } from "axios";
-
 import apiClient from "@/services/api-client";
 import { useQuery } from "@tanstack/react-query";
 import type { AxiosRequestConfig } from "axios";
-
-// interface FetchResponse<T> {
-//   count: number;
-//   results: T[];
-// }
-
-// const useData = <T>(
-//   endpoint: string,
-//   requestConfig?: AxiosRequestConfig,
-//   deps?: unknown[] | undefined,
-// ) => {
-//   const [data, setData] = useState<T[]>([]);
-//   const [error, setError] = useState("");
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(
-//     () => {
-//       setLoading(true);
-//       const controller = new AbortController();
-
-//       apiClient
-//         .get<FetchResponse<T>>(endpoint, {
-//           signal: controller.signal,
-//           ...requestConfig,
-//         })
-//         .then(({ data: { results } }) => {
-//           //
-//           setData(results);
-//           setLoading(false);
-//         })
-//         .catch((e) => {
-//           if (e instanceof CanceledError) return;
-//           setError(e.message);
-//           setLoading(false);
-//         });
-
-//       // ? : checks truthiness (covers all falsy values: false, 0, "", null, undefined, NaN).
-//       // ?? checks specifically for null or undefined.
-//       // ?. accesses a property or calls a method only if the value before it is not null or undefined; otherwise, it returns undefined.
-//       return () => controller.abort();
-
-//       // can also be :[...(deps ?? [])]
-//     },
-//     deps ? [...deps] : [],
-//   );
-
-//   return { data, setData, error, setError, loading, setLoading };
-// };
-
-// export default useData;
 
 interface FetchResponse<T> {
   count: number;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,29 +1,4 @@
-// import apiClient from "@/services/api-client";
-// import { useQuery } from "@tanstack/react-query";
-
 import useData from "./useData";
-
-// export interface Genre {
-//   id: number;
-//   name: string;
-//   slug: string;
-//   image_background: string;
-// }
-
-// interface GenreResponse {
-//   count: number;
-//   results: Genre[];
-// }
-
-// const useGenres = () => {
-//   return useQuery<Genre[], Error>({
-//     queryKey: ["genres"],
-//     queryFn: () =>
-//       apiClient.get<GenreResponse>("/genres").then((res) => res.data.results),
-//   });
-// };
-
-// export default useGenres;
 
 export interface Genre {
   id: number;
