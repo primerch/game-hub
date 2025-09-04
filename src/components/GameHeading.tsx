ad5fbbaac65002525b2b0bab804435358b623d13
Reacher <macryze@icloud.com>
2025-09-04 13:29:54 +0800

    refactor(components): improve game heading component
    - Update GameHeading component to use external data for genre and platform names
    - Replace direct access to gameQuery properties with more robust name lookup
    - Improve type safety and readability by using optional chaining

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,12 +1,22 @@
-import type { GameQuery } from "../App.tsx";
+import type { GameQuery } from '../App.tsx';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
+import genres from '../data/genres.ts';
+import platforms from '../data/platforms.ts';
+
 const GameHeading = ({ gameQuery }: Props) => {
+  const { platform_id, genre_id } = gameQuery;
+
+  const genre_name = genres.find((genre) => genre.id === genre_id)?.name;
+  const platform_name = platforms.find(
+    (platform) => platform.id === platform_id,
+  )?.name;
+
   return (
-    <h1 className="text-5xl font-bold">{`${gameQuery.platform?.name ?? ""} ${gameQuery.genre?.name ?? ""} Games`}</h1>
+    <h1 className="text-5xl font-bold">{`${platform_name ?? ''} ${genre_name ?? ''} Games`}</h1>
   );
 };
 
