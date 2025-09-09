7bc41d936d0a94e0d60a643e37c874e23c9b2202
Reacher <macryze@icloud.com>
2025-09-09 15:02:42 +0800

    refactor(components): extract GameAttributes as a separate component
    - Create a new GameAttributes component to encapsulate game attributes display
    - Move platforms, metascore, genres, and publishers display from GameDetailPage to GameAttributes
    -

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,37 @@
+import CriticScore from '@/components/CriticScore';
+import DefinitionItem from '@/components/DefinitionItem';
+import type { Game } from '@/entities/Game';
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <div className="grid grid-cols-2">
+      <DefinitionItem term={'Platforms'}>
+        {game.parent_platforms?.map(({ platform }) => (
+          <p key={platform.id}>{platform.name}</p>
+        ))}
+      </DefinitionItem>
+
+      <DefinitionItem term="Metascore">
+        <CriticScore metacritic={game.metacritic} />
+      </DefinitionItem>
+
+      <DefinitionItem term="Genres">
+        {game.genres.map((genre) => (
+          <p>{genre.name}</p>
+        ))}
+      </DefinitionItem>
+
+      <DefinitionItem term="Publishers">
+        {game.publishers?.map((publisher) => (
+          <p key={publisher.id}>{publisher.name}</p>
+        ))}
+      </DefinitionItem>
+    </div>
+  );
+};
+
+export default GameAttributes;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,8 +1,7 @@
-import CriticScore from '@/components/CriticScore';
-import DefinitionItem from '@/components/DefinitionItem';
 import ExpandableText from '@/components/ExpandableText';
 import useGame from '@/hooks/useGame';
 import { useParams } from 'react-router';
+import GameAttributes from '../components/GameAttributes';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -18,27 +17,7 @@ const GameDetailPage = () => {
     <>
       <h1 className="text-3xl"> {game.name}</h1>
       <ExpandableText>{game.description_raw}</ExpandableText>
-
-      <div className="align-center grid justify-center lg:grid-cols-2 xl:grid-cols-4">
-        <DefinitionItem term={'Platforms'}>
-          {game.parent_platforms.map((pp) => (
-            <p>{pp.platform.name}</p>
-          ))}
-        </DefinitionItem>
-
-        <DefinitionItem term="Metascore">
-          <CriticScore metacritic={game.metacritic} />
-        </DefinitionItem>
-
-        <DefinitionItem term="Genres">
-          {game.genres.map((genre) => genre.name)}
-        </DefinitionItem>
-        <DefinitionItem term="Publishers">
-          {game.publishers.map((publisher) => (
-            <p>{publisher.name}</p>
-          ))}
-        </DefinitionItem>
-      </div>
+      <GameAttributes game={game} />
     </>
   );
 };
